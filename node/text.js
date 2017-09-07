0b782ca0ea3a722ce2255be8ec642fe71eab7b1f
DESKTOP-NEDMMDF\CL <772319837@qq.com>
2017-09-07 16:22:11 +0800

    node 和 翻译的一部分

---
+++ node/text.js
@@ -0,0 +1,56 @@
+//配置服务器地址
+const http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
+const mysql = require('mysql');
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '123456',
+    database: 'mmi',
+});
+connection.connect();
+//nodejs文件系统模块
+const fs = require('fs');
+//加载sql语句
+const nodejsSqlStatement = require('./js/sql');
+const login = require('./js/getUserData');
+const addUser = require('./js/addUser');
+const server = http.createServer((req, res) => {
+    //设置返回数据类型 解决跨域问题
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.writeHead(200, { "Content-Type": "application/json" });
+    //获取ajax拿到的api接口的参数
+    var inIt = nodejsSqlStatement.url(req, res);
+    if (inIt.url_info.pathname === '/login') {
+        login.getUserData(connection, nodejsSqlStatement.selectUserSQL(), inIt.param, res);
+    } else if (inIt.url_info.pathname === '/add') {
+        addUser.addUserData(connection, nodejsSqlStatement, inIt, res);
+    } else if (inIt.url_info.pathname === '/post') {
+        var json=[],req=res;
+        http.get('http://xtk.azurewebsites.net/BingDictService.aspx?Word=name', (res) => {
+            res.setEncoding('utf-8');
+            let rawData = '';
+            res.on('data', (chunk) => { rawData += chunk; });
+            res.on('end', () => {
+                try {
+                    const parsedData = JSON.parse(rawData);
+                    console.log(parsedData);
+                } catch (e) {
+                    console.error(e.message);
+                }
+                req.end(rawData);
+            });
+        }).on('error', (e) => {
+            console.error(`错误: ${e.message}`);
+        })
+    } else {
+        res.end();
+    }
+});
+server.listen(port, hostname, () => {
+    console.log(`服务器运行在 http://${hostname}:${port}/`);
+});
+server.on('close', function () {
+    connection.end();
+});
--- src/js/basis.js
+++ src/js/basis.js
@@ -14,17 +14,31 @@ function judgment(value,that){
     if(value==="clear"){
         that.clear();
     }else if(value.split(" ")[0]==="random"){
+        //随机数
         value=value.split(" ")[1];
         value?temp.push(parseInt(Math.random()*value+1)):temp.push('miss');
         tempPush(that,temp)
-    }
-    else if(value==="history"){
+    }else if(value.split(" ")[0]==="F"){
+        //翻译
+        value=value.split(" ")[1]
+        var url='http://xtk.azurewebsites.net/BingDictService.aspx?Word='+value;
+        fetch('http://127.0.0.1:3000/post').then(res=>{
+            console.log("Response succeeded?", JSON.stringify(res.ok));
+            console.log(JSON.stringify(res));
+            // res.json().then(data=>{
+            //     console.log(data);
+            // })
+        })
+    }else if(value==="history"){
+        //历史
         temp=temp.concat(stringHistory,history,['-----------------------'])
         tempPush(that,temp)
     }else if(parseFloat(value).toString()!=="NaN"){
+        //数字
         temp.push(value)
         tempPush(that,temp)
     }else{
+        //不存在
         temp.push('miss')
         tempPush(that,temp)
     }
@@ -34,3 +48,4 @@ function tempPush(that,value){
         consoleShow:value
     });
 }
+
